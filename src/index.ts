9630ab107964359250f2d0a6a6ee3e8dc2aabd77
Dolan <dolan_miu@hotmail.com>
2018-05-14 23:06:29 +0100

    Using request instead

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-// tslint:disable:no-any
 import "babel-polyfill";
-import * as Nightmare from "nightmare";
-const nightmare = new Nightmare({ show: false });
+
+import * as request from "request-promise";
 
 module.exports = (context, cb) => {
-    nightmare
-        .goto("https://duckduckgo.com")
-        .type("#search_form_input_homepage", "github nightmare")
-        .click("#search_button_homepage")
-        .wait("#r1-0 a.result__a")
-        .evaluate(() => (document.querySelector("#r1-0 a.result__a") as any).href)
-        .end()
-        .then((data) => {
-            cb(null, { hello: data });
-        })
-        .catch((error) => {
-            console.error("Search failed:", error);
-        });
-    // cb(null, { hello: "data" });
+    const data = context.data || "";
 
+    request
+        .post({
+            url: "http://mirror1.convertonlinefree.com",
+            encoding: null,
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36",
+            },
+            formData: {
+                __EVENTTARGET: "",
+                __EVENTARGUMENT: "",
+                __VIEWSTATE: "",
+                ctl00$MainContent$fu: {
+                    value: data,
+                    options: {
+                        filename: "output.docx",
+                        contentType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                    },
+                },
+                ctl00$MainContent$btnConvert: "Convert",
+                ctl00$MainContent$fuZip: "",
+            },
+        })
+        .then((body) => {
+            cb(null, { valid: body });
+        })
+        .catch(() => {
+            cb(null, { valid: false });
+        });
 };
